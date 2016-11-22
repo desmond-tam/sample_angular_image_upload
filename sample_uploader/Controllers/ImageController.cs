83c76d1abbf60532f8d264ab76884facd094cb9b
Desmond Tam <desmondeds@hotmail.com>
2016-11-22 21:54:53 +1100

    image upload sample

---
+++ sample_uploader/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace sample_uploader.Controllers
+{
+    public class ImageController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            string _filename = "";
+            byte[] _image = null;
+            Stream stream = null;
+
+            stream = HttpContext.Current.Request.InputStream;
+            //int invoiceId = int.Parse(HttpContext.Current.Request.Form["invoiceId"]);
+
+            HttpPostedFile postedFile = HttpContext.Current.Request.Files[0];
+            stream = postedFile.InputStream;
+            _filename = Path.GetFileName(HttpContext.Current.Request.Files[0].FileName);
+
+            byte[] buffer = new byte[stream.Length];
+            stream.Read(buffer, 0, buffer.Length);
+            //          Request.Files[0].InputStream.Read(buffer, 0, Request.Files[0].ContentLength);
+            //byte[] _buffer = ImageHelper.Picture_CheckSizing(buffer, "640");
+            _image = buffer;
+
+            return Ok();
+        }
+    }
+}
---
+++ sample_uploader/js/directive.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+app.directive('uploadPanel', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            action: '@'
+        },
+        templateUrl: 'js/ng_templates/uploader.html',
+        replace: false,
+        controller: ['$scope', function ($scope) {
+            $scope.progress = 0;
+            $scope.avatar = '';
+            $scope.sendFile = function (el) {
+                var $form = $(el).parents('form');
+                if ($(el).val() == '') {
+                    return false;
+                }
+                $form.attr('action', $scope.action);
+
+                $scope.$apply(function () {
+                    $scope.progress = 0;
+                });
+
+                $form.ajaxSubmit({
+                    type: 'POST',
+                    uploadProgress: function (evt, pos, tot, percComplete) {
+                        $scope.$apply(function () {
+                            // upload the progress bar during the upload
+                            $scope.progress = percComplete;
+                        });
+                    },
+                    error: function (evt, statusText, response, form) {
+                        // remove the action attribute from the form
+                        $form.removeAttr('action');
+                        /*
+                          handle the error ...
+                        */
+                    },
+                    success: function (response, status, xhr, form) {
+                        var ar = $(el).val().split('\\'),
+                          filename = ar[ar.length - 1];
+                        // remove the action attribute from the form
+                        $form.removeAttr('action');
+                        $scope.$apply(function () {
+                            //$scope.avatar = filename;
+                            $scope.progress = 0;
+                            $scope.$parent.imageId = response;
+                            $scope.$parent.imageLoaded = true;
+                        });
+                    },
+                });
+            }
+
+        }],
+        link: function (scope, elem, attrs, ctrl) {
+            // link function
+            elem.find(".uploadbtn").click(function () {
+                elem.find('input[type="file"]').click();
+            })
+
+        }
+    }
+});
